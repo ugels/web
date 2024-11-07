f8675b8ac68acac2c5dde3834e2d5db834c4b451
Konk32 <131777454+Konk32@users.noreply.github.com>
2024-11-07 11:35:13 +0100

    More fixes - runs now with http

--- server/server.js
+++ server/server.js
@@ -1,28 +1,26 @@
 // Import necessary modules
 const {
   express,
+  http,
   https,
   socketIO,
   cors,
   fs,
   corsOptions,
-} = require("../loadModules");
+} = require("./loadModules");
 
 const path = require("path");
 const cookieParser = require("cookie-parser");
 
 // Load SSL certificate and key files
 const options = {
-  key: fs.readFileSync("cert/ugels.com_key.key"), // Path to your private key
-  cert: fs.readFileSync("cert/ugels.com.crt"), // Path to your certificate
-  ca: fs.readFileSync("cert/ugels.com.ca-bundle"), // Path to your CA bundle
 };
 
 // Main Express app setup
 const app = express();
 console.log("\x1b[32m%s\x1b[0m", "Express connected to app successfully");
 
-const server = https.createServer(options, app); // Create HTTPS server
+const server = http.createServer(options, app); // Create HTTPS server
 console.log("\x1b[32m%s\x1b[0m", "App loaded successfully");
 
 const io = socketIO(server); // Attach Socket.IO to server
@@ -30,7 +28,6 @@ console.log("\x1b[32m%s\x1b[0m", "Socket.IO loaded successfully");
 
 app.use(express.static("public"));
 app.use(cors(corsOptions));
-pp.use(cors(corsOptions));
 
 // Start the main server on port 25565
 const PORT = process.env.PORT || 25565;
